84556312bb896b4d1880ad57d33c51d104d4a88d
jakob gruber <jakob.gruber@seehr.org>
2009-10-02 23:01:03 +0200

    changed to class structure

--- mkwvconf.py
+++ mkwvconf.py
@@ -7,126 +7,152 @@ import os
 from xml import xpath
 from xml.dom.minidom import parse
 
-#change these to the correct pats for your distribution
-xmlPath = '/usr/share/mobile-broadband-provider-info/serviceproviders.xml'
-configPath = '/etc/wvdial.conf'
+class Mkwvconf:
 
-def clrScr():
-    os.system('clear')
+  #########
+  # file paths
+  #########
 
-def getCountryCode():
-    """prints introduction, then displays a list of country codes contained in the provider database from which the user must choose one"""
-    l = []
-    nodes = getNodesFromXml("country/@code")
-    for n in nodes:
-        l.append(str(n.value))
-    clrScr()
+  xmlPath = '/usr/share/mobile-broadband-provider-info/serviceproviders.xml'
+  configPath = '/etc/wvdial.conf'
+  
+  #########
+  # class members
+  #########
 
-    displayIntroduction()
+  parameters = {}
+  providers = {}
+  countryCode = ""
+  chosenProvider = ""
 
-    print "\nAvailable country codes:\n"
-    print l
-    
-    input = ""
-    
-    while not input in l:
-        input = raw_input("\nGet providers for which country code? : ")
-    return input
-    
-def getNodesFromXml(xquery):
-    """returns results of xquery as a list"""
-    doc = parse(xmlPath)
-    return xpath.Evaluate(xquery, doc.documentElement)
+  introMessage = """
+mkwvconf automatically generates a dialer section for use in wvdial.conf based on mobile-broadband-provider-info.
 
-def getProviders(countryCode):
-    """returns a dictionary of providers for the specified country code"""
-    nodes = getNodesFromXml('country[@code=\'' + countryCode +  '\']/provider/name')
-    d = {}
-    i = 0
-    for n in nodes:
-        d[i] = n.firstChild.nodeValue
-        i+=1
-    return d
-    
-def chooseProvider(providers):
-    """returns provider chosen by user"""
-    max =0
-    clrScr()
-    print "\nProviders for '" + countryCode + "':\n"
-    for k, v in providers.items():
-        print str(k) + ": " + v
-        if k > max:
-            max = k
-            
-    input = -1
-        
-    while input > max or input < 0:
-        inputStr = getUserInput("Choose a provider [0-" + str(max) + "]:")
-        try:
-            input = string.atoi(inputStr)
-            if input < 0 or input > max:
-                print "Input needs to be between 0 and " + str(max)
-        except:
-            input = -1
-            print "Input needs to be an integer."
-        
-    return  providers[int(input)]
-    
-def makeConfig(countryCode, chosenProvider):
-    """assembles configuration section using information provided by user. the configuration is either written to wvdial.conf or printed for manual insertion"""
-    nodes = getNodesFromXml("country[@code='" + countryCode +  "']/provider[name='" + chosenProvider + "']")
-    parameters = parseProviderNode(nodes[0])
-    
-    parameters["modem"] =  getModemDevice()
-    parameters["profileName"] = getUserInput("Enter name for configuration: ","DefaultProfile")
+If a provider is missing from the list, add a bug at http://bugzilla.gnome.org/enter_bug.cgi?product=NetworkManager (include your provider name, your country, your plans marketing name if you know it, and of course the APN you're using for data). For more information about the mobile broadband provider database, see http://blogs.gnome.org/dcbw/2009/06/22/mobile-broadband-assistant-makes-it-easy/ .
 
-    editConf = raw_input("\nDo you want me to try to modify " + configPath + " (you will need superuser rights)? Y/n: ")
-    clrScr()
-    if editConf in ["", "Y",  "y"]:
-        writeConfig(parameters)
-    else:
-        print "\n\nDone. Insert the following into " + configPath + " and run 'wvdial " + parameters["profileName"] + "' to start the connection.\n\n"
-        print formatConfig(parameters)
-    
-def writeConfig(parameters):    
-    """append or replace the configuration section to wvdial.conf"""
-    if not os.path.exists(configPath):
-        print "\nWarning: " + configPath + " doesn't exist, creating new file."        
-        f = open(configPath, 'w')
-        f.close()
-    
-    f = open(configPath, 'r')
-    text = f.read()
-    f.close()
-    
-    snippetStart = text.find("[Dialer %(profileName)s]" % parameters)
-    if snippetStart != -1:
-        snippetEnd = text.find("[Dialer ", snippetStart+1)
-        print "\nThe following part of wvdial.conf will be replaced: \n\n" + text[snippetStart:snippetEnd]
-        print "by: \n\n" + formatConfig(parameters)
-        text = text.replace(text[snippetStart:snippetEnd], formatConfig(parameters))
-    else:
-        print "\nThe following will be appended to wvdial.conf: \n\n" + formatConfig(parameters)
-        text += "\n" + formatConfig(parameters)
-    
-    editConf = raw_input("Write to file? Y/n: ")
-    if editConf in ["", "Y",  "y"]:
-        f = open(configPath, 'w')
-        f.write(text)
-        f.close()
-        
-        print "wvdial.conf edited successfully, run 'wvdial " + parameters["profileName"] + "' to start the connection.\n\n"
-    
-def formatConfig(parameters):
-    """formats the information contained in parameters into a valid wvdial.conf format"""
-    
-    if not 'usr' in parameters:
-        parameters['usr'] = ""
-        
-    if not 'pw' in parameters:
-        parameters['pw'] = ""
-    
-    return \
+The configuration generated by mkwvconf overwrites CID 1 with your provider info ('Init2=AT+CGDCONT=1,...') which is then called by dialing *99***1# (the digit before the trailing '#' specifies which CID to use).
+
+Further reading on APNs can be found here: http://mail.gnome.org/archives/networkmanager-list/2008-August/msg00191.html. Thanks goes to Antti Kaijanmäki for explanations and links!"""
+
+  #########
+  # class methods
+  #########
+
+  def __init__(self):
+	self.doc = parse(self.xmlPath)
+
+  def displayIntro(self):
+	os.system('clear')
+	print self.introMessage
+
+  def getCountryCode(self):
+	"""displays a list of country codes contained in the provider database from which the user must choose one"""
+	l = []
+	nodes = self.getNodesFromXml("country/@code")
+	for n in nodes:
+		l.append(str(n.value))
+
+	print "\nAvailable country codes:\n"
+	print l
+
+	input = ""
+
+	while not input in l:
+		input = raw_input("\nGet providers for which country code? : ")
+
+	self.countryCode = input
+	  
+  def getNodesFromXml(self, xquery):
+	"""returns results of xquery as a list"""
+	return xpath.Evaluate(xquery, self.doc.documentElement)
+
+  def getProviders(self):
+	"""fills dictionary of providers for the specified country code"""
+	nodes = self.getNodesFromXml('country[@code=\'' + self.countryCode +  '\']/provider/name')
+	i = 0
+	for n in nodes:
+		self.providers[i] = n.firstChild.nodeValue
+		i+=1
+	  
+  def chooseProvider(self):
+	"""returns provider chosen by user"""
+	max =0
+	os.system('clear')
+	print "\nProviders for '" + self.countryCode + "':\n"
+	for k, v in self.providers.items():
+		print str(k) + ": " + v
+		if k > max:
+			max = k
+			
+	input = -1
+		
+	while input > max or input < 0:
+		inputStr = self.getUserInput("Choose a provider [0-" + str(max) + "]:")
+		try:
+			input = string.atoi(inputStr)
+			if input < 0 or input > max:
+				print "Input needs to be between 0 and " + str(max)
+		except:
+			input = -1
+			print "Input needs to be an integer."
+		
+	self.chosenProvider = self.providers[int(input)]
+	  
+  def makeConfig(self):
+	"""get final information from user and assembles configuration section. the configuration is either written to wvdial.conf or printed for manual insertion"""
+	nodes = self.getNodesFromXml("country[@code='" + self.countryCode +  "']/provider[name='" + self.chosenProvider + "']")
+	self.parseProviderNode(nodes[0])
+
+	self.parameters["modem"] = self.getModemDevice()
+	self.parameters["profileName"] = self.getUserInput("Enter name for configuration: ","DefaultProfile")
+
+	editConf = raw_input("\nDo you want me to try to modify " + self.configPath + " (you will need superuser rights)? Y/n: ")
+	os.system('clear')
+	if editConf in ["", "Y",  "y"]:
+		self.writeConfig()
+	else:
+		print "\n\nDone. Insert the following into " + self.configPath + " and run 'wvdial " + self.parameters["profileName"] + "' to start the connection.\n\n"
+		print self.formatConfig()
+	  
+  def writeConfig(self):    
+	"""append or replace the configuration section to wvdial.conf"""
+	if not os.path.exists(self.configPath):
+		print "\nWarning: " + self.configPath + " doesn't exist, creating new file."        
+		f = open(self.configPath, 'w')
+		f.close()
+
+	f = open(self.configPath, 'r')
+	text = f.read()
+	f.close()
+
+	snippetStart = text.find("[Dialer %(profileName)s]" % self.parameters)
+	if snippetStart != -1:
+		snippetEnd = text.find("[Dialer ", snippetStart+1)
+		print "\nThe following part of wvdial.conf will be replaced: \n\n" + text[snippetStart:snippetEnd]
+		print "by: \n\n" + self.formatConfig()
+		text = text.replace(text[snippetStart:snippetEnd], self.formatConfig())
+	else:
+		print "\nThe following will be appended to wvdial.conf: \n\n" + self.formatConfig()
+		text += "\n" + self.formatConfig()
+
+	editConf = raw_input("Write to file? Y/n: ")
+	if editConf in ["", "Y",  "y"]:
+		f = open(self.configPath, 'w')
+		f.write(text)
+		f.close()
+		
+		print "wvdial.conf edited successfully, run 'wvdial " + self.parameters["profileName"] + "' to start the connection.\n\n"
+	  
+  def formatConfig(self):
+	"""formats the information contained in parameters into a valid wvdial.conf format"""
+
+	if not 'usr' in self.parameters:
+		self.parameters['usr'] = ""
+		
+	if not 'pw' in self.parameters:
+		self.parameters['pw'] = ""
+
+	return \
 """[Dialer %(profileName)s]
 Modem Type = Analog Modem
 Phone = *99***1#
@@ -138,65 +164,59 @@ Modem = %(modem)s
 Init1 = ATZ
 Init2 = at+cgdcont=1,"ip","%(apn)s"
 Stupid Mode = 1
-""" % parameters
-    
-def getModemDevice():
-    """return modem location provided by user"""
-    defaultLocation = "/dev/ttyUSB0"
-    input = "initialValue"
-    
-    while not input.startswith("/dev/") or len(input) == 0:
-        input = getUserInput("Enter modem location (default is /dev/ttyUSB0): ",  defaultLocation)
-    
-    if len(input.strip()) == 0:
-        input = defaultLocation
-    
-    return input
-    
-def getUserInput(prompt, default=""):
-    """utility method for getting user input. displays prompt, optional default fallback"""
-    accept = "n"
-    inp = ""
-    while accept == "n" or accept == "N":
-        inp = raw_input("\n" + prompt)
-        if len(inp.strip()) == 0:
-            inp = default
-        accept = raw_input("Your choice: '" + inp + "'. Is this correct? Y/n: ")
-    return inp
+""" % self.parameters
+	  
+  def getModemDevice(self):
+	"""return modem location provided by user"""
+	defaultLocation = "/dev/ttyUSB0"
+	input = "initialValue"
 
-def parseProviderNode(node):
-    """fill parameter dictionary from provider xml node"""
-    d = {}
-    
-    apnNode = node.getElementsByTagName("apn")[0]
-    apn = apnNode.getAttribute("value")
-    d["apn"] = apn
-    
-    usrNodes = apnNode.getElementsByTagName("username")
-    if len(usrNodes) != 0:
-        usr = usrNodes[0].firstChild.nodeValue
-        d["usr"] = usr
-    
-    pwNodes = apnNode.getElementsByTagName("password")
-    if len(pwNodes) != 0:
-        pw = pwNodes[0].firstChild.nodeValue
-        d["pw"] = pw
-    
-    return d
+	while not input.startswith("/dev/") or len(input) == 0:
+		input = self.getUserInput("Enter modem location (default is /dev/ttyUSB0): ",  defaultLocation)
 
-def displayIntroduction():
-    print u"""
-mkwvconf automatically generates a dialer section for use in wvdial.conf based on mobile-broadband-provider-info.
+	if len(input.strip()) == 0:
+		input = defaultLocation
 
-If a provider is missing from the list, add a bug at http://bugzilla.gnome.org/enter_bug.cgi?product=NetworkManager (include your provider name, your country, your plans marketing name if you know it, and of course the APN you're using for data). For more information about the mobile broadband provider database, see http://blogs.gnome.org/dcbw/2009/06/22/mobile-broadband-assistant-makes-it-easy/ .
+	return input
+	  
+  def getUserInput(self, prompt, default=""):
+	"""utility method for getting user input. displays prompt, optional default fallback"""
+	accept = "n"
+	inp = ""
+	while accept == "n" or accept == "N":
+		inp = raw_input("\n" + prompt)
+		if len(inp.strip()) == 0:
+			inp = default
+		accept = raw_input("Your choice: '" + inp + "'. Is this correct? Y/n: ")
+	return inp
 
-The configuration generated by mkwvconf overwrites CID 1 with your provider info ('Init2=AT+CGDCONT=1,...') which is then called by dialing *99***1# (the digit before the trailing '#' specifies which CID to use).
+  def parseProviderNode(self, node):
+	"""fill parameter dictionary from provider xml node"""
+	self.parameters = {}
+	
+	apnNode = node.getElementsByTagName("apn")[0]
+	apn = apnNode.getAttribute("value")
+	self.parameters["apn"] = apn
+	
+	usrNodes = apnNode.getElementsByTagName("username")
+	if len(usrNodes) != 0:
+		usr = usrNodes[0].firstChild.nodeValue
+		self.parameters["usr"] = usr
+	
+	pwNodes = apnNode.getElementsByTagName("password")
+	if len(pwNodes) != 0:
+		pw = pwNodes[0].firstChild.nodeValue
+		self.parameters["pw"] = pw
 
-Further reading on APNs can be found here: http://mail.gnome.org/archives/networkmanager-list/2008-August/msg00191.html. Thanks goes to Antti Kaijanmäki for explanations and links!
-    """
+  def getProviderFromUser(self):
+	self.getCountryCode()
+	self.getProviders()
+	self.chooseProvider()
 
 if __name__ == "__main__":
-    countryCode = getCountryCode()
-    providers = getProviders(countryCode)
-    chosenProvider = chooseProvider(providers)
-    makeConfig(countryCode, chosenProvider)
+
+  mkwvconf = Mkwvconf()
+
+  mkwvconf.displayIntro()
+  mkwvconf.getProviderFromUser()
+  mkwvconf.makeConfig()
